0657dcf0ce8ce4200337f5b58122aaa570f7a617
Dmitry Mordanov <Mordanov314@gmail.com>
2022-03-01 23:24:59 +0300

    task13 completed

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая позволяет выяснить, кратно ли число заданному, если нет, вывести остаток.
+using System;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите число, на которое будем проверять кратность числа {number}: ");
+int kratn = Convert.ToInt32(Console.ReadLine());
+
+if (number % kratn == 0)
+{
+    Console.WriteLine($"Число {number} кратно числу {kratn}");
+}
+else
+{
+    Console.Write($"Число {number} не кратно числу {kratn}. ");
+    Console.WriteLine($"Остаток от деления {number} на {kratn} равен {number % kratn}");
+}
