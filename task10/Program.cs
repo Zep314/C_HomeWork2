cb0787dd46df443f7eed5cae25ed8f638acd48e5
Dmitry Mordanov <Mordanov314@gmail.com>
2022-03-01 23:09:09 +0300

    task10 completed

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+using System;
 
-namespace task10
+Console.Write("Введите трехзначное число (от 100 до 999): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>99 && number<1000)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    number /= 10;  // Отбрасываем самую правую цифру числа
+    number %= 10;  // Отсекаем самую левую цифру числа
+
+    Console.WriteLine($"Вторая цифра введенного числа: {number}");
+}
+else
+{
+    Console.WriteLine("Введено неверное число!");
 }
